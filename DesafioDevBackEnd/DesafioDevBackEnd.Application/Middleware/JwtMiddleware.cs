aa82dae33a4e9ec1767930fd33cd24082507c551
Fabrício Marcondes <fmarcondes@mellowoodmedical.com>
2022-04-11 23:39:51 -0300

    Configuring swagger documentation and authorization

---
+++ DesafioDevBackEnd/DesafioDevBackEnd.Application/Middleware/JwtMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DesafioDevBackEnd.Application.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+                await attachUserToContextAsync(context, token);
+
+            await _next(context);
+        }
+
+        private async Task attachUserToContextAsync(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler(); 
+                using (var provider = new System.Security.Cryptography.RSACryptoServiceProvider(2048))
+                {
+                    var key = new RsaSecurityKey(provider.ExportParameters(true));
+               
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                        ClockSkew = TimeSpan.Zero
+                    }, out SecurityToken validatedToken);
+                }
+            }
+            catch
+            {
+                // do nothing if jwt validation fails
+                // user is not attached to context so request won't have access to secure routes
+            }
+        }
+    }
+}
--- DesafioDevBackEnd/DesafioDevBackEnd/Startup.cs
+++ DesafioDevBackEnd/DesafioDevBackEnd/Startup.cs
@@ -12,6 +12,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DesafioDevBackEnd.Application.Extensions;
+using System.Reflection;
+using System.IO;
+using DesafioDevBackEnd.Application.Middleware;
 
 namespace DesafioDevBackEnd
 {
@@ -26,6 +29,48 @@ namespace DesafioDevBackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Swagger - Enable this line and the related lines in Configure method to enable swagger UI
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "DesafioDevBackEndAPI", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                //options.CustomSchemaIds(x => x.FullName);
+
+                //Note: This is just for showing Authorize button on the UI. 
+                //Authorize button's behaviour is handled in wwwroot/swagger/ui/index.html
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+                options.EnableAnnotations();
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -51,7 +96,8 @@ namespace DesafioDevBackEnd
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
